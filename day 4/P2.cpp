e82aafb164f65537fd3e0e268a706ad56a9f4ff4
austinkor <119714461+austinkor@users.noreply.github.com>
2022-12-06 13:30:41 +0800

    Add files via upload

---
+++ day 4/P2.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+int main(){
+    fstream file("input.txt");
+    char temp;
+    string text,num,ff,fs,sf,ss;
+    int arrofindex[3];
+    int index,point,firststart,secstart,firstsec,secsec;
+    bool condition;
+    int sum = 0;
+
+    while(getline(file,text)){
+        num = "";
+        ff = "";
+        fs = "";
+        ss = "";
+        sf = "";
+        condition = false;
+        index = 0;
+        point = 0;
+        while (index < text.length()){
+            temp = text[index];
+            if (isdigit(temp)){
+                num += temp;
+            }
+            else{
+                arrofindex[point]=num.length()-1;
+                point++;
+            }
+
+            index++;
+        }
+        index = 0;
+        while (index < num.length()){
+            if (index <= arrofindex[0]){
+                ff += num[index];
+            }
+            else if (index <= arrofindex[1]){
+                fs += num[index];
+            }
+            else if (index <= arrofindex[2]){
+                sf += num[index];
+            }
+            else {
+                ss += num[index];
+            }
+            index ++;
+        }
+
+        firststart = stoi(ff);
+        firstsec = stoi(fs);
+        secsec = stoi(ss);
+        secstart = stoi(sf);
+
+        if (not (firststart > secsec||secstart>firstsec)){
+            condition = true;
+        }
+        if (condition){
+            sum++;
+        }
+    }
+    cout << sum;
+    return 0;
+}
+
